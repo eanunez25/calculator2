a4d5d7ff661e2bb0f5876b78c4e47429af0a7530
Elvin <eanunez25@gmail.com>
2023-08-10 23:17:03 -0500

    add operate and displayValue functions

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ let add = (x, y) => x + y
 let subtract = (x, y) => x - y
 let multiply = (x, y) => x * y
 let divide = (x, y) => x / y
+
+let firstNum
+let secondNum
+let operator
+
+function operate(x, y, operation) {
+  if (operation == "+") {
+    return add(x, y)
+  } else if (operation == "-") {
+    return subtract(x, y)
+  } else if (operation == "*") {
+    return multiply(x, y)
+  } else {
+    return divide(x, y)
+  }
+}
+
+function displayValue(firstNum, secondNum, operator) {
+  return `${firstNum} ${operator} ${secondNum}`
+}
